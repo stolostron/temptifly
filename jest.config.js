0ba7d26bec6216ff062f88b8f7aabe5446b826e0
John Swanke <jswanke@redhat.com>
2020-11-20 10:21:49 -0500

    backup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
-/*******************************************************************************
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
- * * Copyright (c) 2020 Red Hat, Inc.
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
- *******************************************************************************/
-
 const tapReporter = [
   'jest-tap-reporter',
   {
