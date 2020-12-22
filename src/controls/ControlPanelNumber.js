75835bf64c788578272d6e9799f158169906d75f
John Swanke <jswanke@redhat.com>
2020-12-22 11:05:24 -0500

    backup

--- src/controls/ControlPanelNumber.js
+++ src/controls/ControlPanelNumber.js
@@ -2,8 +2,8 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { NumberInput } from 'carbon-components-react'
-import Tooltip from '../components/Tooltip'
+import { FormGroup, Popover } from '@patternfly/react-core'
+import HelpIcon from '@patternfly/react-icons/dist/js/icons/help-icon'
 
 class ControlPanelNumber extends React.Component {
   static propTypes = {
@@ -23,46 +23,98 @@ class ControlPanelNumber extends React.Component {
   };
 
   render() {
-    const { controlId, i18n, control } = this.props
-    const { name, initial, exception, validation={} } = control
+    const { controlId, i18n, control, handleChange } = this.props
+    const { name, initial, exception, validation={}, tooltip } = control
+    let { active } = control
+    active = active || initial
+    const nActive = parseInt(active, 10)
 
+    const onChange = (value) => {
+      control.active = value.toString()
+      handleChange()
+    }
+
+    const validated = exception ? 'error' : undefined
     return (
       <React.Fragment>
         <div
           className="creation-view-controls-number"
           ref={this.setControlRef.bind(this, control)}
         >
-          <label
-            className="creation-view-controls-multiselect-title"
-            htmlFor={controlId}
+          <FormGroup
+            id={`${controlId}-label`}
+            label={name}
+            isRequired={validation.required}
+            fieldId={controlId}
+            helperTextInvalid={exception}
+            validated={validated}
+            labelIcon={
+              /* istanbul ignore next */
+              tooltip ? (
+                <Popover
+                  id={`${controlId}-label-help-popover`}
+                  bodyContent={tooltip}
+                >
+                  <button
+                    id={`${controlId}-label-help-button`}
+                    aria-label="More info"
+                    onClick={(e) => e.preventDefault()}
+                    className="pf-c-form__group-label-help"
+                  >
+                    <HelpIcon noVerticalAlign />
+                  </button>
+                </Popover>
+              ) : (
+                <React.Fragment />
+              )
+            }
           >
-            {name}
-            {validation.required ? (
-              <div className="creation-view-controls-required">*</div>
-            ) : null}
-            <Tooltip control={control} i18n={i18n} />
-          </label>
-          <NumberInput
-            allowEmpty
-            id={controlId}
-            value={typeof initial === 'string' ? Number(initial) : initial}
-            invalid={!!exception}
-            invalidText={exception}
-            min={1}
-            max={100}
-            step={1}
-            onChange={this.handleChange.bind(this, control)}
-          />
+            <div className="pf-c-number-input">
+              <div className="pf-c-input-group">
+                <button className="pf-c-button pf-m-control" style={{lineHeight: '16px'}} type="button" aria-label="Minus"
+                  onClick={()=>{
+                    onChange(nActive-1)
+                  }}>
+                  <span className="pf-c-number-input__icon">
+                    <svg height="16" width="16" role="img" viewBox="0 0 24 24">
+                      <path d="M0 10h24v4h-24z" />
+                    </svg>
+                  </span>
+                </button>
+                <input
+                  className="pf-c-form-control"
+                  type="number"
+                  value={active}
+                  name="number-input-default-name"
+                  onFocus={e => {
+                    e.target.select()
+                  }}
+                  onChange={e => {
+                    onChange(e.target.value)
+                  }}
+                  aria-label="Number input"
+                />
+                <button className="pf-c-button pf-m-control" style={{lineHeight: '16px'}} type="button" aria-label="Plus"
+                  onClick={()=>{
+                    onChange(nActive+1)
+                  }}>
+                  <svg height="16" width="16" role="img" viewBox="0 0 24 24">
+                    <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            {validated === 'error' ? (
+              <div style={{ borderTop: '1.75px solid red', paddingBottom: '6px', maxWidth: '400px' }}></div>
+            ) : (
+              <React.Fragment />
+            )}
+          </FormGroup>
         </div>
       </React.Fragment>
     )
   }
 
-  handleChange(id, evt) {
-    const { control, handleChange } = this.props
-    control.active = evt.imaginaryTarget.valueAsNumber
-    handleChange(evt)
-  }
 }
 
 export default ControlPanelNumber
