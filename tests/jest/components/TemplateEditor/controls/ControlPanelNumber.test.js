ae30b8e42f980e055d305eeeda093adf1961e291
John Swanke <jswanke@redhat.com>
2021-02-05 15:35:02 -0500

    number

--- tests/jest/components/TemplateEditor/controls/ControlPanelNumber.test.js
+++ tests/jest/components/TemplateEditor/controls/ControlPanelNumber.test.js
@@ -2,22 +2,46 @@
 
 import React from 'react'
 import ControlPanelNumber from '../../../../../src/controls/ControlPanelNumber'
-import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-import { control } from '../../TestingData'
+export const control =
+{
+  name: 'creation.app.name',
+  tooltip: 'tooltip.creation.app.name',
+  controlData: [],
+  id: 'name',
+  type: 'number',
+}
+const fn = jest.fn()
 
 describe('ControlPanelNumber component', () => {
   it('renders as expected', () => {
-    const fn = jest.fn()
-    const component = renderer.create(
-      <ControlPanelNumber
-        key={'key'}
-        control={control}
-        controlId={'controlId'}
-        handleChange={fn}
-        i18n={fn}
-      />
-    )
-    expect(component.toJSON()).toMatchSnapshot()
+
+    const Component = () => {
+      return (
+        <ControlPanelNumber
+          key={'key'}
+          control={control}
+          controlId={'controlId'}
+          handleChange={fn}
+          i18n={fn}
+        />
+      )
+    }
+    const { getByTestId, asFragment, rerender } = render(<Component />)
+    expect(asFragment()).toMatchSnapshot()
+
+    userEvent.type(getByTestId('numberinput'), '3')
+    expect(control.active).toBe('3')
+    userEvent.click(getByTestId('numberup'))
+    expect(control.active).toBe('4')
+    control.active = '0'
+    control.exception = 'error'
+    rerender(<Component />)
+    expect(asFragment()).toMatchSnapshot()
+    userEvent.click(getByTestId('numberdn'))
+    expect(control.active).toBe('0')
+
   })
 })
