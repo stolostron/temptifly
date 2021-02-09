534e018c53f1c92f9a875c1fa106a96353ee2dae
John Swanke <jswanke@redhat.com>
2021-02-09 16:50:19 -0500

    backup

--- example/controlData/ControlData.js
+++ example/controlData/ControlData.js
@@ -1,116 +1,84 @@
-'use strict'
+import React from 'react'
+import fs from 'fs'
+import path from 'path'
+import Handlebars from 'handlebars'
+import configHbs from '../templates/config.hbs'
 
-import gitChannelData from './ControlDataGit'
+import controlDataBMC from './ControlDataBMC'
+import { BaremetalLogo } from './Logos'
+import { VALID_DNS_LABEL } from '../../src/utils/validation-types'
 
-import {
-  discoverGroupsFromSource,
-  shiftTemplateObject
-} from '../transformers/transform-resources-to-controls'
+const installConfig =
+    typeof configHbs !== 'string'
+      ? configHbs
+      : Handlebars.compile(fs.readFileSync(path.resolve(__dirname, '../templates/config.hbs'), 'utf8'))
 
-import gitSvg from '../graphics/git-repo.svg'
-const VALID_DNS_LABEL = '^[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?$'
-
-const controlDataExample = [
+const controlData = [
   {
     id: 'main',
     type: 'section',
-    note: 'creation.view.required.mark'
+    title: 'creation.ocp.cluster.details',
+    collapsable: true,
   },
   {
-    name: 'creation.app.name',
-    tooltip: 'tooltip.creation.app.name',
+    name: 'creation.ocp.name',
+    tooltip: 'tooltip.creation.ocp.name',
     id: 'name',
     type: 'text',
     validation: {
       constraint: VALID_DNS_LABEL,
       notification: 'import.form.invalid.dns.label',
-      required: true
+      required: true,
     },
-    reverse: 'Application[0].metadata.name'
+    reverse: 'ClusterDeployment[0].metadata.name',
   },
   {
-    name: 'creation.app.namespace',
-    tooltip: 'tooltip.creation.app.namespace',
-    id: 'namespace',
-    type: 'combobox',
-    validation: {
-      constraint: VALID_DNS_LABEL,
-      notification: 'import.form.invalid.dns.label',
-      required: true
-    },
-    reverse: 'Application[0].metadata.namespace'
-  },
-  {
-    id: 'userDefinedNamespace',
+    id: 'showSecrets',
     type: 'hidden',
-    active: ''
+    active: false,
   },
-  ////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////  channels  /////////////////////////////////////
+
+  ///////////////////////  container platform  /////////////////////////////////////
   {
-    id: 'channelSection',
+    id: 'chooseDist',
     type: 'section',
-    title: 'creation.app.channels',
+    title: 'creation.ocp.distribution',
+    info: 'creation.ocp.choose.distribution',
+    tooltip: 'tooltip.creation.ocp.choose.distribution',
     overline: true,
     collapsable: true,
-    collapsed: false
   },
-  ///////////////////////  channels  /////////////////////////////////////
+  ///////////////////////  cloud  /////////////////////////////////////
   {
-    id: 'channels',
-    type: 'group',
-    prompts: {
-      nameId: 'channelPrompt',
-      baseName: 'resource',
-      addPrompt: 'creation.app.add.channel',
-      deletePrompt: 'creation.app.delete.channel'
-    },
-    discover: discoverGroupsFromSource,
-    shift: shiftTemplateObject,
-    controlData: [
+    id: 'chooseInfra',
+    type: 'title',
+    tooltip: 'tooltip.creation.ocp.choose.aws.infrastructure',
+    learnMore: 'https://docs.openshift.com/container-platform/4.3/installing/',
+  },
+  {
+    id: 'infrastructure',
+    type: 'cards',
+    sort: false,
+    pauseControlCreationHereUntilSelected: true,
+    scrollViewAfterSelection: 300,
+    available: [
       {
-        id: 'channel',
-        type: 'section',
-        title: 'creation.app.channel.title',
-        collapsable: true,
-        collapsed: false,
-      },
-      ///////////////////////  channel name  /////////////////////////////////////
-      {
-        id: 'channelPrompt',
-        type: 'hidden',
-        active: ''
-      },
-      {
-        id: 'selfLinks',
-        type: 'hidden',
-        active: ''
-      },
-      {
-        id: 'channelType',
-        type: 'cards',
-        sort: false,
-        collapseCardsControlOnSelect: true,
-        scrollViewToTopOnSelect: true,
-        title: 'creation.app.channel.type',
-        collapsable: true,
-        collapsed: false,
-        available: [
-          {
-            id: 'github',
-            logo: gitSvg,
-            title: 'channel.type.git',
-            tooltip: 'tooltip.creation.app.channel.git',
-            change: {
-              insertControlData: gitChannelData
-            }
+        id: 'BMC',
+        logo: <BaremetalLogo />,
+        title: 'cluster.create.baremetal.subtitle',
+        change: {
+          insertControlData: controlDataBMC,
+          replacements: {
+            'install-config': { template: installConfig, encode: true, newTab: true },
           },
-        ],
-        active: '',
-        validation: {}
-      }
-    ]
-  }
+        },
+      },
+    ],
+    validation: {
+      notification: 'creation.ocp.cluster.must.select.infrastructure',
+      required: true,
+    },
+  },
 ]
 
-export default controlDataExample
+export default controlData
---
+++ example/controlData/ControlDataBMC.js
@@ -0,0 +1,324 @@
+import {
+  LOAD_OCP_IMAGES,
+  networkingControlData,
+  labelControlData,
+} from './ControlDataHelpers'
+import {
+  ControlMode,
+} from '../../src/utils/source-utils'
+import {
+  VALIDATE_CIDR,
+  VALIDATE_IP_AGAINST_MACHINE_CIDR,
+  VALIDATE_IP_AGAINST_MACHINE_CIDR_OPTIONAL,
+  VALIDATE_ALPHANUMERIC,
+  VALIDATE_ALPHANUMERIC_PERIOD,
+  VALIDATE_MAC_ADDRESS,
+} from '../../src/utils/validation-types'
+//import { listBareMetalAssets } from '../../../../../resources/bare-metal-asset'
+import _ from 'lodash'
+
+const setAvailableBMAs = (control, result) => {
+  const { loading } = result
+  const { data } = result
+  const bmas = data
+  if (!control.isLoaded) {
+    control.available = []
+    control.isLoading = false
+    const error = bmas ? null : result.error
+    if (error) {
+      control.isFailed = true
+    } else if (bmas) {
+      control.isLoaded = true
+      control.active = []
+      control.available = bmas
+        .filter((bma) => {
+          return !_.get(bma, 'spec.clusterDeployment.name')
+        })
+        .map(formatBMA)
+        .sort(({ hostName: a }, { hostName: b }) => {
+          return a.localeCompare(b)
+        })
+      control.available.forEach((datum) => {
+        datum.id = datum.id.toString()
+      })
+    } else {
+      control.isLoading = loading
+    }
+  }
+}
+
+const formatBMA = (bma) => ({
+  id: bma.metadata.uid,
+  macAddress: bma.spec.bootMACAddress,
+  credName: bma.spec.bmc.credentialsName,
+  credNamespace: bma.metadata.namespace,
+  bmcAddress: bma.spec.bmc.address,
+  hostName: bma.metadata.name,
+  hostNamespace: bma.metadata.namespace,
+})
+
+const sortTable = (items, selectedKey, sortDirection, active) => {
+  if (selectedKey === 'role' && active.length > 0) {
+    const sorting = ['master', 'worker', 'unactive']
+    const activeMap = _.keyBy(active, 'id')
+    items.sort(({ id: a }, { id: b }) => {
+      if (activeMap[a] && !activeMap[b]) {
+        return -1
+      } else if (!activeMap[a] && activeMap[b]) {
+        return 1
+      } else {
+        return (
+          (sorting.indexOf(_.get(activeMap[a], 'role', 'unactive')) -
+                        sorting.indexOf(_.get(activeMap[b], 'role', 'unactive'))) *
+                    (sortDirection === 'asc' ? 1 : -1)
+        )
+      }
+    })
+    return items
+  }
+  return _.orderBy(items, [selectedKey], [sortDirection])
+}
+
+const validateTable = (active = []) => {
+  let master = 0
+  active.forEach(({ role }) => {
+    if (role === 'master') {
+      master++
+    }
+  })
+  if (master < 3) {
+    return 'creation.ocp.validation.errors.hosts'
+  }
+  return null
+}
+
+const getActiveRole = (active = []) => {
+  let master = 0
+  active.forEach(({ role }) => {
+    if (role === 'master') {
+      master++
+    }
+  })
+  return master < 3 ? 'master' : 'worker'
+}
+
+const WorkerInstanceTypes = [
+  {
+    label: 'General Purpose',
+    children: [
+      {
+        label: 'E2 machine types',
+        children: [
+          {
+            label: 'E2 standard machine types',
+            children: [
+              { value: 'e2-standard-2', description: '2 vCPU, 8 GiB RAM - General Purpose' },
+              { value: 'e2-standard-4', description: '4 vCPU, 16 GiB RAM - General Purpose' },
+              { value: 'e2-standard-8', description: '8 vCPU, 32 GiB RAM - General Purpose' },
+              { value: 'e2-standard-16', description: '16 vCPU, 64 GiB RAM - General Purpose' },
+            ],
+          },
+          {
+            label: 'E2 high-memory machine types',
+            children: [
+              { value: 'e2-highmem-2', description: '2 vCPU, 16 GiB RAM - General Purpose' },
+              { value: 'e2-highmem-4', description: '4 vCPU, 32 GiB RAM - General Purpose' },
+              { value: 'e2-highmem-8', description: '8 vCPU, 64 GiB RAM - General Purpose' },
+              { value: 'e2-highmem-16', description: '16 vCPU, 128 GiB RAM - General Purpose' },
+            ],
+          },
+          {
+            label: 'E2 high-CPU machine types',
+            children: [
+              { value: 'e2-highcpu-2', description: '2 vCPU, 2 GiB RAM - General Purpose' },
+              { value: 'e2-highcpu-4', description: '4 vCPU, 4 GiB RAM - General Purpose' },
+              { value: 'e2-highcpu-8', description: '8 vCPU, 8 GiB RAM - General Purpose' },
+              { value: 'e2-highcpu-16', description: '16 vCPU, 16 GiB RAM - General Purpose' },
+            ],
+          },
+        ],
+      },
+    ]
+  }]
+
+const controlDataBMC = [
+  ////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////  imageset  /////////////////////////////////////
+  {
+    name: 'cluster.create.ocp.image',
+    tooltip: 'tooltip.cluster.create.ocp.image',
+    id: 'imageSet',
+    type: 'combobox',
+    placeholder: 'creation.ocp.cloud.select.ocp.image',
+    fetchAvailable: LOAD_OCP_IMAGES('bmc'),
+    validation: {
+      notification: 'creation.ocp.cluster.must.select.ocp.image',
+      required: true,
+    },
+  },
+
+  ////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////  connection  /////////////////////////////////////
+  ...labelControlData,
+  {
+    name: 'creation.ocp.instance.type',
+    tooltip: 'tooltip.creation.ocp.gcp.instance.type',
+    learnMore: 'https://cloud.google.com/compute/docs/machine-types',
+    id: 'workerType',
+    type: 'treeselect',
+    available: WorkerInstanceTypes,
+    active: 'n1-standard-4',
+    validation: {
+      constraint: '[A-Za-z0-9-]+',
+      notification: 'creation.ocp.cluster.valid.alphanumeric.period',
+      required: false,
+    },
+    cacheUserValueKey: 'create.cluster.worker.type',
+  },
+
+  ///////////////////////  hosts  /////////////////////////////////////
+  {
+    id: 'hostsSections',
+    type: 'section',
+    title: 'creation.ocp.cluster.hosts',
+    tooltip: 'tooltip.creation.ocp.cluster.hosts',
+    overline: true,
+    collapsable: true,
+    shadowed: true,
+  },
+  {
+    id: 'chooseHosts',
+    type: 'title',
+    info: 'creation.ocp.choose.hosts',
+  },
+  {
+    id: 'hosts',
+    type: 'table',
+    validation: {
+      tester: validateTable.bind(null),
+    },
+    sortTable,
+    summaryKey: 'hostName', // when table is collapsed, collapsed summary is composed of a list of this table value
+    controlData: [
+      ///////////////////////  host name  /////////////////////////////////////
+      {
+        name: 'creation.ocp.host.name',
+        id: 'hostName',
+        type: 'text',
+        validation: VALIDATE_ALPHANUMERIC,
+      },
+      {
+        name: 'creation.ocp.host.namespace',
+        id: 'hostNamespace',
+        type: 'text',
+        validation: VALIDATE_ALPHANUMERIC,
+      },
+      {
+        name: 'creation.ocp.host.role',
+        id: 'role',
+        type: 'toggle',
+        active: getActiveRole,
+        available: ['master', 'worker'],
+        validation: {
+          notification: 'creation.ocp.cluster.valid.key',
+          required: true,
+        },
+      },
+      {
+        name: 'creation.ocp.host.bmc.address',
+        id: 'bmcAddress',
+        type: 'text',
+      },
+      {
+        name: 'creation.ocp.host.mac.address',
+        id: 'macAddress',
+        type: 'text',
+        validation: VALIDATE_MAC_ADDRESS,
+        mode: ControlMode.PROMPT_ONLY,
+      },
+      {
+        name: 'creation.ocp.host.user',
+        id: 'username',
+        type: 'text',
+        active: '# injected by server',
+        validation: VALIDATE_ALPHANUMERIC,
+        mode: ControlMode.PROMPT_ONLY,
+      },
+      {
+        name: 'creation.ocp.host.password',
+        id: 'password',
+        type: 'password',
+        active: '# injected by server',
+        mode: ControlMode.PROMPT_ONLY,
+      },
+    ],
+    fetchAvailable: {
+      //      query: () => {
+      //        return listBareMetalAssets().promise
+      //      },
+      loadingDesc: 'table.bma.loading',
+      setAvailable: setAvailableBMAs,
+    },
+    active: [],
+  },
+  {
+    id: 'disableCertificateVerification',
+    type: 'checkbox',
+    name: 'creation.ocp.host.disable.certificate.verification',
+    tooltip: 'tooltip.creation.ocp.host.disable.certificate.verification',
+    active: 'true',
+    available: ['false', 'true'],
+  },
+  ...networkingControlData,
+  {
+    id: 'provisioningNetworkCIDR',
+    type: 'text',
+    name: 'creation.ocp.network.cidr',
+    tooltip: 'tooltip.creation.ocp.network.cidr',
+    active: '',
+    validation: VALIDATE_CIDR,
+  },
+  {
+    id: 'provisioningNetworkInterface',
+    type: 'text',
+    name: 'creation.ocp.network.interface',
+    tooltip: 'tooltip.creation.ocp.network.interface',
+    active: 'enp1s0',
+    validation: VALIDATE_ALPHANUMERIC,
+  },
+  {
+    id: 'provisioningNetworkBridge',
+    type: 'text',
+    name: 'creation.ocp.network.bridge',
+    tooltip: 'tooltip.creation.ocp.network.bridge',
+    active: 'provisioning',
+    validation: VALIDATE_ALPHANUMERIC_PERIOD,
+  },
+  {
+    id: 'externalNetworkBridge',
+    type: 'text',
+    name: 'creation.ocp.external.bridge',
+    tooltip: 'tooltip.creation.ocp.external.bridge',
+    active: 'baremetal',
+    validation: VALIDATE_ALPHANUMERIC_PERIOD,
+  },
+  {
+    id: 'dnsVIP',
+    type: 'text',
+    name: 'creation.ocp.dns.vip',
+    hidden: 'true',
+    tooltip: 'tooltip.creation.ocp.dns.vip',
+    active: '',
+    validation: VALIDATE_IP_AGAINST_MACHINE_CIDR,
+  },
+  {
+    id: 'apiVIP',
+    type: 'text',
+    name: 'creation.ocp.api.vip',
+    tooltip: 'tooltip.creation.ocp.api.vip',
+    active: '',
+    validation: VALIDATE_IP_AGAINST_MACHINE_CIDR_OPTIONAL,
+  },
+]
+
+export default controlDataBMC
