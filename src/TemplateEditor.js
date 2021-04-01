3255fa0b2a7798881e4ff07afc67d74a8c37a472
John Swanke <jswanke@redhat.com>
2021-04-01 11:56:10 -0400

    fix 2nd tab edit bug (#59)

--- src/TemplateEditor.js
+++ src/TemplateEditor.js
@@ -1034,7 +1034,7 @@ export default class TemplateEditor extends React.Component {
         templateYAML: newYAML,
         templateObject,
         templateResources: tr
-      } = generateSource(template, editStack, controlData, otherYAMLTabs)
+      } = generateSource(template, {}, controlData, otherYAMLTabs)
       highlightChanges(this.editors[0], oldYAML, newYAML)
       this.setState({
         controlData,
