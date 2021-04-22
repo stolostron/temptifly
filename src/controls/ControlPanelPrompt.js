b69add023d08be4a0af18026c90094b54bc173e0
John Swanke <jswanke@redhat.com>
2021-04-22 13:38:06 -0400

    fix build

--- src/controls/ControlPanelPrompt.js
+++ src/controls/ControlPanelPrompt.js
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BrowserRouter, Link } from 'react-router-dom'
 
 class ControlPanelPrompt extends React.Component {
   static propTypes = {
@@ -19,10 +18,6 @@ class ControlPanelPrompt extends React.Component {
       return (
         <React.Fragment key={id}>{this.renderLink(control)}</React.Fragment>
       )
-    case 'popup':
-      return (
-        <React.Fragment key={id}>{this.renderPopup(control)}</React.Fragment>
-      )
     }
     return null
   }
@@ -32,27 +27,9 @@ class ControlPanelPrompt extends React.Component {
     const { prompt, url, id } = prompts
     const { i18n } = this.props
     const text = i18n(prompt)
-    return (
-      <React.Fragment>
-        <div className="creation-view-controls-add-value-container bottom-right">
-          <BrowserRouter forceRefresh={true}>
-            <Link to={url} id={id} className="creation-view-controls-add-button">
-              {text}
-            </Link>
-          </BrowserRouter>
-        </div>
-      </React.Fragment>
-    )
-  }
-
-  renderPopup(control) {
-    const { prompts } = control
-    const { prompt, url, id } = prompts
-    const { i18n } = this.props
-    const text = i18n(prompt)
 
     const createPopupWindow = () => {
-      window.open(`${window.location.origin}${url}`, '${id}' )
+      window.open(`${window.location.origin}${url}`, `${id}` )
     }
 
     return (
