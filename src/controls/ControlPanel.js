da7e1db5768ede177081baa5a25560959b273b32
John Swanke <jswanke@redhat.com>
2022-06-06 09:25:36 -0400

    test (#192)
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- src/controls/ControlPanel.js
+++ src/controls/ControlPanel.js
@@ -166,7 +166,7 @@ class ControlPanel extends React.Component {
         }
       }
     })
-    // if no steps, just do a form test testing
+    // if no steps, just do a form
     if (steps.length === 0) {
       return this.renderControlForm(sections, controlClasses)
     } else {
